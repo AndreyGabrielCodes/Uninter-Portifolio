b6efc325a7de9f9b1e0a2be3b42efd822d2c9bf3
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-04-18 22:20:16 -0300

    Criado mensagem de sucesso ao enviar formulario

--- Scripts/Formulario.js
+++ Scripts/Formulario.js
@@ -1,8 +1,14 @@
 const formulario = document.getElementById('Formulario');
+const mensagem = document.getElementById("MensagemSucesso");
 
 formulario.addEventListener('submit', function(event){
     if (this.checkValidity()){
         //Impede o comportamento padrão quando não possui destino (voltar ao topo)
-        //event.preventDefault();
+        event.preventDefault();
+        mensagem.style.setProperty('display','block');
     }
 })
+
+function FecharMensagemSucesso(){
+    mensagem.style.setProperty('display','none');
+}
